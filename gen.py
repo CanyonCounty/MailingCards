db49a50cf4762b7da15b378fee83361a61c8218b
Kenneth Wilcox <wilcoxk@gmail.com>
2014-10-01 13:23:46 -0600

    Merged with test.py to create pdf properties

--- gen.py
+++ gen.py
@@ -1,9 +1,31 @@
 from pyPdf import PdfFileWriter, PdfFileReader
+from pyPdf import *
 import sys
 
 fname = "PB44.pdf"
 
 output = PdfFileWriter()
+
+# Let's not forget the pdf properties
+# Store the last one
+item = output._objects.pop()
+# delete the one we don't care about (send to the last one)
+output._objects.pop()
+# Create a new object for the Properties...
+properties = pdf.DictionaryObject()
+properties.update ({
+  pdf.NameObject("/Title"): pdf.createStringObject(u"Nothing to see here"),
+  pdf.NameObject("/Producer"): pdf.createStringObject(u"Python PDF Library - http://pybrary.net/pyPdf/"),
+  pdf.NameObject("/Author"): pdf.createStringObject(u"Kenneth Wilcox"),
+  pdf.NameObject("/Subject"): pdf.createStringObject(u"Headaches are fun"),
+  pdf.NameObject("/Creator"): pdf.createStringObject(u"Ken's gen.py script"),
+  pdf.NameObject("/Keywords"): pdf.createStringObject(u"what the hell - does this work")
+  })
+# Add It
+output._addObject(properties)
+# Add back the one we deleted first
+output._addObject(item)
+
 address = PdfFileReader(file("input\\address\\"+fname, "rb"))
 location = PdfFileReader(file("input\\location\\"+fname, "rb"))
 
@@ -19,9 +41,6 @@ for i in xrange(address.numPages):
   #print ".",
   output.addPage(address.getPage(i))
   output.addPage(page)
-#  if i > 4:
-#    break
-#print ""
 
 # output should be double address
 print "Output Pages:", output.getNumPages()
